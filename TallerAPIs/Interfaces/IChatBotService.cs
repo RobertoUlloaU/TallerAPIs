cf31d90bcc03ea17cf70a735577e706ef5536e18
Roberto Alejandro Ulloa Urbina <roberto.ulloa@udla.edu.ec>
2025-05-18 16:10:25 -0500

    Agregar interfaz IChatBotService con métodos para obtener respuesta del chatbot y guardar respuestas localmente

---
+++ TallerAPIs/Interfaces/IChatBotService.cs
@@ -0,0 +1,8 @@
+﻿namespace TallerAPIs.Interfaces
+{
+    public interface IChatBotService
+    {
+        public Task <string> ObtenerRespuestaChatBot(string promt);
+        public bool GuardarRespuestaBaseDatosLocal(string promt,string respuesta);
+    }
+}
